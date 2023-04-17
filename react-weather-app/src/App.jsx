3cc543023f6b882555885a74bbce01f2afb66f60
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-04-17 07:51:16 +0100

    Weather API added

--- react-weather-app/src/App.jsx
+++ react-weather-app/src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css';
 import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import { useState } from 'react';
+
+
+
+
 function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.split(" ")
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    const foreCastFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+
+    Promise.all([currentWeatherFetch, foreCastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+      })
+
+    setCurrentWeather(weatherResponse)
+
   }
   return (
     <div className='container'>
--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -8,3 +8,7 @@ export const geoApiOptions = {
 
 
 export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "b73c4f5d6fddbd6cffa40fcf8a74a2c3"
