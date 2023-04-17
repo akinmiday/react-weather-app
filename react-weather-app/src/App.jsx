6efa8c3d069b33f36eef33f04a3a9e6fe7a835d3
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-04-17 06:47:38 +0100

    initial

---
+++ react-weather-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import CurrentWeather from './components/current-weather/current-weather';
+import Search from './components/search/search';
+function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+  return (
+    <div className='container'>
+      <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
+    </div>
+  )
+}
+
+export default App
---
+++ react-weather-app/src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f7f611767fmshfc583ef3a77107dp1dcd41jsn841d5e295b3f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
